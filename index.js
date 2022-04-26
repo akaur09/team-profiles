da45ecf13cc4ecf172259d1ee5f3b1e27ab55a01
akaur091 <amniderkaur.09@gmail.com>
2022-04-25 18:46:33 -0700

    fix test folder

---
+++ index.js
@@ -0,0 +1,5 @@
+// use prompter:
+    // enter team mangagers name, employee ID, Email address,and office number
+    // give option to add enginers info and interns info
+    // give option to add more or end roster.
+    // when prompting ends then html is generated.
